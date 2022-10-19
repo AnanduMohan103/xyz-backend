ca59ab3a3c9fd07f7a002a479b563cbb2df4ecdd
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-19 08:59:10 +0530

    new

---
+++ src/test/java/com/nestditital/xyzbackendapp/XyzBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestditital.xyzbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
